90a80c29e4acd09d0af27144fc0e03089792286b
Junpapadiamond <20040522jun@gmail.com>
2025-03-17 17:33:10 -0400

    uploaded toml file

---
+++ luck_test.py
@@ -0,0 +1,19 @@
+import pytest
+from pypokemon import luckypokemon
+def test_luckypokemon():
+    pokemon_list = [
+        "Pikachu", "Charmander", "Bulbasaur", "Squirtle", "Jigglypuff", 
+        "Meowth", "Psyduck", "Snorlax", "Eevee", "Gyarados", 
+        "Lapras", "Ditto", "Vaporeon", "Jolteon", "Flareon",
+        "Machamp", "Gengar", "Dragonite", "Mewtwo", "Mew",
+        "Togepi", "Espeon", "Umbreon", "Scizor", "Heracross",
+        "Blaziken", "Swampert", "Sceptile", "Gardevoir", "Salamence",
+        "Metagross", "Lucario", "Garchomp", "Infernape", "Torterra",
+        "Empoleon", "Roserade", "Glaceon", "Leafeon", "Togekiss",
+        "Zoroark", "Hydreigon", "Greninja", "Aegislash", "Talonflame",
+        "Sylveon", "Decidueye", "Incineroar", "Primarina", "Zacian"
+    ]
+    for i in range(100):
+        result = luckypokemon()
+        lucky_pokemon = result.split("... ")[1].split("!")[0]  # Extract Pokémon name
+        assert lucky_pokemon in pokemon_list
